6a620ab9c36bc69454cc39c9251e3a45a7441a3a
MartinHeinz <martin7.heinz@gmail.com>
2022-03-19 15:55:40 +0100

    Auth verification command.

--- extractor/extractor.py
+++ extractor/extractor.py
@@ -1,4 +1,6 @@
 import typer
+from googleapiclient.discovery import build
+from google.oauth2 import service_account
 
 extractor = typer.Typer()
 
@@ -12,9 +14,16 @@ def setup():
 
 
 @extractor.command()
-def test():
-    ...
+def auth():
     # Test authentication using generated configuration
+    try:
+        credentials = service_account.Credentials.from_service_account_file("SOME_PATH.json")  # TODO Read this from config
+        scoped_credentials = credentials.with_scopes(['openid'])
+        with build('oauth2', 'v2', credentials=scoped_credentials) as service:
+            user_info = service.userinfo().v2().me().get().execute()
+            typer.echo(f"Successfully authenticated with user: {user_info['id']}")
+    except BaseException as e:
+        typer.echo(f"Authenticated failed with error: '{e}'")
 
 
 @extractor.command()
