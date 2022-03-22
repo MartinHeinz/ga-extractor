89407826dc9e9adb312d7d3b139852e2fd6f5235
MartinHeinz <martin7.heinz@gmail.com>
2022-03-22 21:11:41 +0100

    Transoformation (WIP).

--- ga_extractor/extractor.py
+++ ga_extractor/extractor.py
@@ -20,6 +20,10 @@ class SamplingLevel(str, Enum):
     LARGE = "LARGE"
 
 
+class OutputFormat(str, Enum):
+    csv = "csv"
+
+
 @extractor.command()
 def setup(metrics: str = typer.Option(..., "--metrics"),
           dimensions: str = typer.Option(..., "--dimensions"),
@@ -80,16 +84,16 @@ def auth():
 @extractor.command()
 def extract(report: Optional[Path] = typer.Option("report.json", dir_okay=True)):
     """
-    # Extracts data based on the config
+    Extracts data based on the config
     """
     # https://developers.google.com/analytics/devguides/reporting/core/v4
 
-    # TODO Provide flags for overrides
     app_dir = typer.get_app_dir(APP_NAME)
     config_path: Path = Path(app_dir) / "config.yaml"
     output_path: Path = Path(app_dir) / report
     if not config_path.is_file():
         typer.echo("Config file doesn't exist yet. Please run 'setup' command first.")
+        typer.Exit(2)
     with config_path.open() as file:
         config = yaml.safe_load(file)
         credentials = service_account.Credentials.from_service_account_file(config["serviceAccountKeyPath"])
@@ -110,9 +114,34 @@ def extract(report: Optional[Path] = typer.Option("report.json", dir_okay=True))
                         "metrics": [metrics]
                     }]}
 
+        headers = {}
+        data = {}
         rows = []
         with build('analyticsreporting', 'v4', credentials=scoped_credentials) as service:
             response = service.reports().batchGet(body=body).execute()
+            typer.echo(response)
+            typer.echo()
+            headers = {
+                # e.g. "'ga:browser'+'ga:operatingSystem'"
+                "dimensions": [response["reports"][0]["columnHeader"]["dimensions"]],
+                # e.g. "'Chrome', 'Android'"
+                "metrics": [m["name"] for m in response["reports"][0]["columnHeader"]["metricHeader"]["metricHeaderEntries"]]
+            }
+            data = {
+                "dimensions": [d["dimensions"] for d in response["reports"][0]["data"]["rows"]],
+                "metrics": [v[0]["values"] for v in (m["metrics"] for m in response["reports"][0]["data"]["rows"])]
+            }
+            typer.echo(headers)
+            # {
+            # 'dimensions': [['ga:browser', 'ga:operatingSystem']],
+            # 'metrics': ['ga:sessions', 'ga:bounces']
+            # }
+            typer.echo(data)
+            # {
+            # 'dimensions': [['Android Webview', 'Android'], ['Chrome', 'Android'], ...],
+            # 'metrics': [['1', '1'], ['237', '216'], ...]
+            # }
+
             rows.extend(response["reports"][0]["data"]["rows"])
 
             while "nextPageToken" in response["reports"][0]:
@@ -122,6 +151,46 @@ def extract(report: Optional[Path] = typer.Option("report.json", dir_okay=True))
 
             output_path.write_text(json.dumps(rows))
         typer.echo(f"Report written to {output_path.absolute()}")
+
+
+# TODO: Instead of transformation of adhoc exports, transform only exports that fit into particular output type
+@extractor.command()
+def transform(infile: Optional[Path] = typer.Option("report.json", dir_okay=True),
+              outfile: Optional[Path] = typer.Option(""),
+              outformat: OutputFormat = typer.Option(OutputFormat.csv, "--output-format"),):
+    """
+    Transforms extracted data to other formats (e.g. CSV, SQL)
+    """
+    app_dir = typer.get_app_dir(APP_NAME)
+    input_path: Path = Path(app_dir) / infile  # TODO Allow for absolute path (files outside of config folder)
+    if not input_path.is_file():
+        typer.echo("Input file doesn't exist. Please run 'extract' command first.")
+        typer.Exit(2)
+    stdout = True
+    if outfile:
+        stdout = False
+        output_path: Path = Path(app_dir) / outfile  # TODO Allow for absolute path (files outside of config folder)
+
+    with input_path.open() as infile:
+        # TODO Add missing column names (change in extract first)
+        result = ""
+        if outformat is OutputFormat.csv:
+            # TODO Transform data to CSV matrix
+            report = json.load(infile)
+            for row in report:
+                dims = row["dimensions"]
+                metrics = row["metrics"][0]["values"]
+                typer.echo(f"dims: {dims}, metrics: {metrics}")
+        else:
+            typer.echo(f"Invalid output format: {outformat}")
+            typer.Exit(2)
+
+        if stdout:
+            typer.echo(result)
+        else:
+            with output_path.open(mode="w") as outfile:
+                outfile.write(result)
+                typer.echo(f"Report written to {output_path.absolute()}")
 
 
 @extractor.command()
