12651c0f1abebbdaa01e43f295d17752867aabd5
MartinHeinz <martin7.heinz@gmail.com>
2022-03-24 20:59:15 +0100

    Transformation to SQL (WIP).

--- ga_extractor/extractor.py
+++ ga_extractor/extractor.py
@@ -1,5 +1,6 @@
 import json
 import uuid
+from collections import namedtuple
 
 import typer
 from googleapiclient.discovery import build
@@ -26,15 +27,14 @@ class OutputFormat(str, Enum):
     CSV = "CSV"
     UMAMI = "UMAMI"
 
-    _format_mapping = {
-        JSON: "json",
-        CSV: "csv",
-        UMAMI: "sql",
-    }
-
     @staticmethod
     def file_suffix(f):
-        return OutputFormat._format_mapping[f]
+        format_mapping = {
+            OutputFormat.JSON: "json",
+            OutputFormat.CSV: "csv",
+            OutputFormat.UMAMI: "sql",
+        }
+        return format_mapping[f]
 
 
 @extractor.command()
@@ -148,7 +148,7 @@ def extract(report: Optional[Path] = typer.Option("report.json", dir_okay=True))
 @extractor.command()
 def transform(infile: Optional[Path] = typer.Option("report.json", dir_okay=True),
               outfile: Optional[Path] = typer.Option(""),
-              outformat: OutputFormat = typer.Option(OutputFormat.RAW, "--output-format"),):
+              outformat: OutputFormat = typer.Option(OutputFormat.JSON, "--output-format"),):
     """
     Transforms extracted data to other formats (e.g. CSV, SQL)
     """
@@ -190,18 +190,6 @@ def migrate(outputFormat: OutputFormat = typer.Option(OutputFormat.JSON, "--form
     """
     Export necessary data and transform it to format for target environment (Umami, ...)
     """
-    # For Umami:
-    # INSERT INTO public.website (website_id, website_uuid, user_id, name, domain, share_id, created_at) VALUES (1, '...', 1, 'Blog', 'localhost', '...', '2022-02-22 15:07:31.4+00');
-    # INSERT INTO public.session (session_id, session_uuid, website_id, created_at, hostname, browser, os, device, screen, language, country) VALUES (1, 'fff811c4-8991-5ae3-b4ba-34b75401db54', 1, '2022-02-22 15:14:14.323+00', 'localhost', 'chrome', 'Linux', 'desktop', '1920x1080', 'en', NULL);
-    # INSERT INTO public.session (session_id, session_uuid, website_id, created_at, hostname, browser, os, device, screen, language, country) VALUES (2, 'fd2c990e-11b3-5bbe-9239-dae9556d1161', 1, '2022-02-23 12:03:36.126+00', 'localhost', 'chrome', 'Linux', 'desktop', '1920x1080', 'en', NULL);
-    # INSERT INTO public.pageview (view_id, website_id, session_id, created_at, url, referrer) VALUES (1, 1, 1, '2022-02-22 15:14:14.327+00', '/', '/');
-    # INSERT INTO public.pageview (view_id, website_id, session_id, created_at, url, referrer) VALUES (2, 1, 1, '2022-02-22 15:14:14.328+00', '/', '');
-    # INSERT INTO public.pageview (view_id, website_id, session_id, created_at, url, referrer) VALUES (3, 1, 2, '2022-02-23 12:03:36.135+00', '/blog/53', '');
-    # INSERT INTO public.pageview (view_id, website_id, session_id, created_at, url, referrer) VALUES (4, 1, 2, '2022-02-23 12:04:07.279+00', '/blog/54', '/blog/54');
-    # SELECT pg_catalog.setval('public.pageview_view_id_seq', 4, true);
-    # SELECT pg_catalog.setval('public.session_session_id_seq', 2, true);
-    # SELECT pg_catalog.setval('public.website_website_id_seq', 1, true);
-
     # Query data per day (startDate/endDate same, multiple data ranges can be included in single query)
     # Generate the views + sessions based on the data, ignoring exact visit time
     # Old sessions won't be preserved, bounce rate and session duration won't be accurate; Views and visitors on day-level granularity with be accurate
@@ -218,23 +206,27 @@ def migrate(outputFormat: OutputFormat = typer.Option(OutputFormat.JSON, "--form
         credentials = service_account.Credentials.from_service_account_file(config["serviceAccountKeyPath"])
         scoped_credentials = credentials.with_scopes(['https://www.googleapis.com/auth/analytics.readonly'])
 
-        rows = __migrate_extract(scoped_credentials, config['table'], config['startDate'], config['endDate'])
+        date_ranges = _migrate_date_ranges(config['startDate'], config['endDate'])
+        rows = __migrate_extract(scoped_credentials, config['table'], date_ranges)
         data = __migrate_transform(rows)
 
         output_path.write_text(json.dumps(data))
         typer.echo(f"Report written to {output_path.absolute()}")
 
 
-def __migrate_extract(credentials, table_id, start_date, end_date):
-    dimensions = ["ga:pagePath", "ga:browser", "ga:operatingSystem", "ga:deviceCategory", "ga:browserSize",
-                  "ga:language", "ga:country"]
-    metrics = ["ga:pageviews", "ga:sessions"]
-
+def _migrate_date_ranges(start_date, end_date):
     start_date = datetime.strptime(start_date, '%Y-%m-%d')
     end_date = datetime.strptime(end_date, '%Y-%m-%d')
     date_ranges = [{"startDate": f"{start_date + timedelta(days=d):%Y-%m-%d}",
                     "endDate": f"{start_date + timedelta(days=d):%Y-%m-%d}"} for d in
                    range(((end_date.date() - start_date.date()).days + 1))]
+    return date_ranges
+
+
+def __migrate_extract(credentials, table_id, date_ranges):
+    dimensions = ["ga:pagePath", "ga:browser", "ga:operatingSystem", "ga:deviceCategory", "ga:browserSize",
+                  "ga:language", "ga:country"]
+    metrics = ["ga:pageviews", "ga:sessions"]
 
     body = {"reportRequests": [
         {
@@ -243,17 +235,65 @@ def __migrate_extract(credentials, table_id, start_date, end_date):
             "metrics": [{"expression": m} for m in metrics]
         }]}
 
-    rows = []
+    rows = {}
     for r in date_ranges:
         with build('analyticsreporting', 'v4', credentials=credentials) as service:
             body["reportRequests"][0]["dateRanges"] = [r]
             response = service.reports().batchGet(body=body).execute()
 
-            rows.extend(response["reports"][0]["data"]["rows"])
+            rows[date_ranges["startDate"]] = response["reports"][0]["data"]["rows"]
 
     return rows
 
 
 def __migrate_transform(rows):
     # TODO transform rows to given format (SQL, CSV, Raw JSON)
-    ...
+    # For Umami:
+    # INSERT INTO public.website (website_id, website_uuid, user_id, name, domain, share_id, created_at) VALUES (1, '...', 1, 'Blog', 'localhost', '...', '2022-02-22 15:07:31.4+00');
+    # INSERT INTO public.session (session_id, session_uuid, website_id, created_at, hostname, browser, os, device, screen, language, country) VALUES (1, 'fff811c4-8991-5ae3-b4ba-34b75401db54', 1, '2022-02-22 15:14:14.323+00', 'localhost', 'chrome', 'Linux', 'desktop', '1920x1080', 'en', NULL);
+    # INSERT INTO public.session (session_id, session_uuid, website_id, created_at, hostname, browser, os, device, screen, language, country) VALUES (2, 'fd2c990e-11b3-5bbe-9239-dae9556d1161', 1, '2022-02-23 12:03:36.126+00', 'localhost', 'chrome', 'Linux', 'desktop', '1920x1080', 'en', NULL);
+    # INSERT INTO public.pageview (view_id, website_id, session_id, created_at, url, referrer) VALUES (1, 1, 1, '2022-02-22 15:14:14.327+00', '/', '/');
+    # INSERT INTO public.pageview (view_id, website_id, session_id, created_at, url, referrer) VALUES (2, 1, 1, '2022-02-22 15:14:14.328+00', '/', '');
+    # INSERT INTO public.pageview (view_id, website_id, session_id, created_at, url, referrer) VALUES (3, 1, 2, '2022-02-23 12:03:36.135+00', '/blog/53', '');
+    # INSERT INTO public.pageview (view_id, website_id, session_id, created_at, url, referrer) VALUES (4, 1, 2, '2022-02-23 12:04:07.279+00', '/blog/54', '/blog/54');
+    # SELECT pg_catalog.setval('public.pageview_view_id_seq', 4, true);
+    # SELECT pg_catalog.setval('public.session_session_id_seq', 2, true);
+    # SELECT pg_catalog.setval('public.website_website_id_seq', 1, true);
+
+    # {'dimensions': ['/blog/29', 'Opera', 'Windows', 'desktop', '2520x1320', 'de-de', 'Germany'], 'metrics': [{'values': ['4', '1']}]}
+    # Notes: there can be 0 sessions in the record; there's always more or equal number of views
+    #        - treat zero sessions as one
+    #        - if sessions is non-zero and page views are > 1, then divide, e.g.:
+    #           - 5, 5 - 5 sessions, 1 view each
+    #           - 4, 2 - 2 sessions, 1 view each
+    #           - 5, 3 - 3 sessions, 2x1 view, 1x3 views
+    website_id = 1
+    hostname = "localhost"
+    PageView = namedtuple('PageView', ["id", "website_id", "session_id", "created_at", "url"])
+    Session = namedtuple('Session', ["session_id", "session_uuid", "website_id", "created_at", "hostname", "browser", "os", "device", "screen", "language", "country"])
+    page_view_id = 1
+    records = []
+    # TODO Loop through sessions/page views (or multiply records)
+    for i, (k, v) in enumerate(rows.items()):
+        s = Session(session_uuid=uuid.uuid4(),
+                    session_id=i,
+                    website_id=website_id,
+                    created_at=k,
+                    hostname=hostname,
+                    browser=v["dimensions"][1],
+                    os=v["dimensions"][2],
+                    device=v["dimensions"][3],
+                    screen=v["dimensions"][4],
+                    language=v["dimensions"][5],
+                    country=v["dimensions"][6])
+        p = PageView(id=page_view_id, website_id=website_id, session_id=i, created_at=k, url=v["dimensions"][0])
+        page_view_id += 1
+        session_insert = (
+            f"INSERT INTO public.session (session_id, session_uuid, website_id, created_at, hostname, browser, os, device, screen, language, country) "
+            f"VALUES ({s.session_id}, {s.session_uuid}, {s.website_id}, {s.created_at}, {s.hostname}, {s.browser}, {s.os}, {s.device}, {s.screen}, {s.language}, {s.country});"
+        )
+        page_insert = f"INSERT INTO public.pageview (view_id, website_id, session_id, created_at, url, referrer) VALUES ({p.id}, {p.website_id}, {p.session_id}, {p.created_at}, {p.url}, NULL);"
+        records.append(session_insert)
+        records.append(page_insert)
+        typer.echo(session_insert)
+        typer.echo(page_insert)
