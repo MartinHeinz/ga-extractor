01cbe129ce698a50c506521ddab4c88b0a415466
MartinHeinz <martin7.heinz@gmail.com>
2022-03-24 17:25:50 +0100

    Full data extraction in date range.

--- ga_extractor/extractor.py
+++ ga_extractor/extractor.py
@@ -1,10 +1,11 @@
 import json
+import uuid
 
 import typer
 from googleapiclient.discovery import build
 from google.oauth2 import service_account
 import yaml
-from datetime import datetime
+from datetime import datetime, timedelta
 from pathlib import Path
 from enum import Enum
 from typing import Optional
@@ -199,8 +200,7 @@ def transform(infile: Optional[Path] = typer.Option("report.json", dir_okay=True
 
 # TODO
 @extractor.command()
-def migrate(outfile: Optional[Path] = typer.Option(""),
-            outputFormat: OutputFormat = typer.Option(OutputFormat.RAW, "--format"),):
+def migrate(outputFormat: OutputFormat = typer.Option(OutputFormat.RAW, "--format")):
     """
     Export necessary data and transform it to format for target environment (Umami, ...)
     """
@@ -216,13 +216,44 @@ def migrate(outfile: Optional[Path] = typer.Option(""),
     # SELECT pg_catalog.setval('public.session_session_id_seq', 2, true);
     # SELECT pg_catalog.setval('public.website_website_id_seq', 1, true);
 
-    # RT metrics and dims https://developers.google.com/analytics/devguides/reporting/realtime/dimsmets
-    # RT goes only 30 min into past and does not provide data for individual sessions or page views
-
-    # https://ga-dev-tools.web.app/dimensions-metrics-explorer
-    # Dims: ga:pagePath, (ga:browser, ga:operatingSystem, ga:deviceCategory, ga:browserSize, ga:language, ga:country), Metrics: ga:pageviews, (ga:sessions)
     # Query data per day (startDate/endDate same, multiple data ranges can be included in single query)
     # Generate the views + sessions based on the data, ignoring exact visit time
     # Old sessions won't be preserved, bounce rate and session duration won't be accurate; Views and visitors on day-level granularity with be accurate
-    # TODO Export, Transform, Insert, Test
+    # TODO Transform, Insert, Test
 
+    app_dir = typer.get_app_dir(APP_NAME)
+    config_path: Path = Path(app_dir) / "config.yaml"
+    # output_path: Path = Path(app_dir) / f"{uuid.uuid4()}_extract.json"
+    if not config_path.is_file():
+        typer.echo("Config file doesn't exist yet. Please run 'setup' command first.")
+        typer.Exit(2)
+    with config_path.open() as file:
+        config = yaml.safe_load(file)
+        credentials = service_account.Credentials.from_service_account_file(config["serviceAccountKeyPath"])
+        scoped_credentials = credentials.with_scopes(['https://www.googleapis.com/auth/analytics.readonly'])
+
+        dimensions = ["ga:pagePath", "ga:browser", "ga:operatingSystem", "ga:deviceCategory", "ga:browserSize", "ga:language", "ga:country"]
+        metrics = ["ga:pageviews", "ga:sessions"]
+
+        start_date = datetime.strptime(config['startDate'], '%Y-%m-%d')
+        end_date = datetime.strptime(config['endDate'], '%Y-%m-%d')
+        date_ranges = [{"startDate": f"{start_date + timedelta(days=d):%Y-%m-%d}", "endDate": f"{start_date + timedelta(days=d):%Y-%m-%d}"} for d in range(((end_date.date() - start_date.date()).days + 1))]
+
+        body = {"reportRequests": [
+            {
+                "viewId": f"{config['table']}",
+                "dimensions": [{"name": d} for d in dimensions],
+                "metrics": [{"expression": m} for m in metrics]
+            }]}
+
+        rows = []
+        for r in date_ranges:
+
+            with build('analyticsreporting', 'v4', credentials=scoped_credentials) as service:
+                body["reportRequests"][0]["dateRanges"] = [r]
+                response = service.reports().batchGet(body=body).execute()
+
+                rows.extend(response["reports"][0]["data"]["rows"])
+
+        # output_path.write_text(json.dumps(rows))
+        # typer.echo(f"Report written to {output_path.absolute()}")
